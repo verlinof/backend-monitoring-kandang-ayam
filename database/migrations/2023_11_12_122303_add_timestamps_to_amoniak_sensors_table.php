582f43dbf3309660ab1643d56a427c66e7a08b81
kafa123 <nurmuhammadkafabih@mail.ugm.ac.id>
2023-11-23 13:22:06 +0700

    hilangkan timestamp

--- database/migrations/2023_11_12_122303_add_timestamps_to_amoniak_sensors_table.php
+++ database/migrations/2023_11_12_122303_add_timestamps_to_amoniak_sensors_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('amoniak_sensors', function (Blueprint $table) {
             //
-            $table->timestamps();
         });
     }
 
